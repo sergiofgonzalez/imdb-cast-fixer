7d50098a4ebac788c9c4c693074087e449c6ae32
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2015-12-14 08:33:09 +0100

    Add Travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,4 +154,7 @@ module.exports = function(grunt) {
   /* release tasks */
   grunt.registerTask("build:release", "Prepares a `release` build in `/build`", ["clean", "jshint", "useminPrepare", "concat:generated", "cssmin:generated", "uglify:generated", "copy:html", "filerev", "usemin", "clean:tmp"]);
   grunt.registerTask("heroku", ["build:release"]);
+
+  /* continuous integration tasks */
+  grunt.registerTask("ci", "Continuous integration tasks", ["jshint"]);
 };
